ad3832ce152d10dfae2c7d5bf46aa4888af76555
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-04-02 17:19:25 +0700

    Add oldest code

--- scraper.sh
+++ scraper.sh
@@ -34,16 +34,42 @@ apkmirror() {
     req $name-v$version.apk $url
 }
 
+apkmirror() {
+    org="$1" name="$2" package="$3" arch="$4" 
+    version=$(req - 2>/dev/null $api | get_supported_version "$package")
+    url="https://www.apkmirror.com/uploads/?appcategory=$name"
+    version="${version:-$(req - $url | get_apkmirror_version | get_latest_version )}"
+    url="https://www.apkmirror.com/apk/$org/$name/$name-${version//./-}-release"
+    url="https://www.apkmirror.com$(req - $url | grep -B5 -A10 '>APK<' | grep -B13 -A2 '>'$arch'<' \
+                                               | grep -B15 '>nodpi</d' | sed -n 's/.*href="\([^"]*\)".*/\1/p;q')"
+    url="https://www.apkmirror.com$(req - $url | grep 'downloadButton' | sed -n 's/.*href="\([^"]*\)".*/\1/p;q')"
+    url="https://www.apkmirror.com$(req - $url | grep 'rel="nofollow"' | sed -n 's/.*href="\([^"]*\)".*/\1/g;s#amp;##g;p;q')"
+    req $name-v$version.apk $url
+}
+
 # X not work (maybe more)
 uptodown() {
     name=$1 package=$2
-    version=$(req - 2>/dev/null "https://api.revanced.app/v2/patches/latest" | get_supported_version "$package")
+    version=$(req - 2>/dev/null $api | get_supported_version "$package")
     url="https://$name.en.uptodown.com/android/versions"
     version="${version:-$(req - 2>/dev/null "$url" | sed -n 's/.*class="version">\([^<]*\)<.*/\1/p' | get_latest_version)}"
     url=$(req - $url | tr '\n' ' ' \
                      | sed -n 's/.*data-url="\([^"]*\)".*'$version'<\/span>[^@]*@\([^<]*\).*/\1/p' \
                      | sed 's#/download/#/post-download/#g')
     url="https://dw.uptodown.com/dwn/$(req - $url | sed -n 's/.*class="post-download".*data-url="\([^"]*\)".*/\1/p')"
+    req $name-v$version.apk $url
+}
+
+uptodown() {
+    name=$1 package=$2
+    version=$(req - 2>/dev/null $api | get_supported_version "$package")
+    url="https://$name.en.uptodown.com/android/versions"
+    version="${version:-$(req - 2>/dev/null "$url" | grep -oP 'class="version">\K[^<]+' | get_latest_version)}"
+    url=$(req - "$url" | grep -B3 '"version">'$version'<' \
+                       | grep -oP 'data-url="\K[^"]*' \
+                       | grep -m 1 "." \
+                       | sed 's/\/download\//\/post-download\//g')
+    url="https://dw.uptodown.com/dwn/$(req - "$url" | grep 'class="post-download"' | grep -oP 'data-url="\K[^"]+')"
     req $name-v$version.apk $url
 }
 
@@ -55,5 +81,15 @@ apkpure() {
     version="${version:-$(req - $url | sed -n 's/.*data-dt-version="\([^"]*\)".*/\1/p' | sed 10q | get_latest_version)}"
     url="https://apkpure.net/$name/$package/download/$version"
     url=$(req - $url | sed -n 's/.*href="\(.*\/APK\/'$package'[^"]*\).*/\1/p' | uniq)
+    req $name-v$version.apk $url
+}
+
+apkpure() {
+    name=$1 package=$2
+    url="https://apkpure.net/$name/$package/versions"
+    version=$(req - 2>/dev/null $api | get_supported_version "$package")
+    version="${version:-$(req - "$url" | grep -oP 'data-dt-version="\K[^"]*' | sed 10q | get_latest_version)}"
+    url="https://apkpure.net/$name/$package/download/$version"
+    url=$(req - "$url" | grep 'Download APK' | grep -oP 'href="\Khttps://d\.apkpure\.net/b/APK[^"]*' | uniq)
     req $name-v$version.apk $url
 }
